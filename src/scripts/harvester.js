8bdae2209ddc5454d1b3a65cb71ae1d84fd2acb3
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2024-08-08 14:58:11 -0500

    Removed ghost code

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,22 +1,6 @@
 export const harvestPlants = (arr) => {
   const harvestedPlants = [];
 
-  // for (const plant of arr) {
-  //   let length = plant.output;
-  //   if (Array.isArray(plant)) {
-  //     for (const corn of plant) {
-  //       let cornLength = corn.output / 2;
-  //       for (let i = 0; i < cornLength; i++) {
-  //         harvestedPlants.push(corn);
-  //       }
-  //     }
-  //   } else {
-  //     for (let i = 0; i < length; i++) {
-  //       harvestedPlants.push(plant);
-  //     }
-  //   }
-  // }
-
   for (const plant of arr) {
     let length = plant.output;
 
